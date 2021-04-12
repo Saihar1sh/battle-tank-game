ce21aa9aeea14f59bf80f6575cdafcb8e0fc6add
Har15h <saiharishmanne@gmail.com>
2021-04-12 22:39:01 +0530

    object pool update

--- Assets/Scripts/Object Pooling/PoolServiceTank.cs
+++ Assets/Scripts/Object Pooling/PoolServiceTank.cs
@@ -7,24 +7,13 @@ public class PoolServiceTank : PoolServiceGeneric<TankController>
 {
     private TankModel tankModel;
     private TankView tankPrefab;
-    private EnemyView enemyPrefab;
 
-    private bool enemyNeeded = false, playerNeeded = false;
 
     public TankController GetTank(TankModel tankModel, TankView tankPrefab, Vector3 vector3, Quaternion rotation)
     {
-        playerNeeded = true;
         this.tankModel = tankModel;
         this.tankPrefab = tankPrefab;
         return GetItem();
-    }
-    public TankController GetEnemy(TankModel tankModel, EnemyView enemyPrefab, Vector3 vector3, Quaternion rotation)
-    {
-        enemyNeeded = true;
-        this.tankModel = tankModel;
-        this.enemyPrefab = enemyPrefab;
-        return GetItem();
-
     }
     protected override TankController CreateItem()
     {
